7f22065001af623bc469c00b79c79b0f24298275
Jordy Aaldering <aalderingjordy@gmail.com>
2024-09-11 15:31:22 +0200

    remove warning disable

--- code/config.mk
+++ code/config.mk
@@ -2,7 +2,7 @@ CXX = clang++
 CC = clang
 
 # Do not use -Ofast / -ffast-math as this may break the evaluation of orient(p, q, p) to exact zero!
-FLAGS = -O3 -march=native -mtune=native -Wall -Wextra -Wno-unused-command-line-argument -Wno-deprecated -Wno-vla-cxx-extension -fopenmp
+FLAGS = -O3 -march=native -mtune=native -Wall -Wextra -Wno-unused-command-line-argument -Wno-deprecated -fopenmp
 DBUG_FLAGS = -fsanitize=address -fsanitize=undefined -ggdb
 FLAGS += $(DBUG_FLAGS)
 LFLAGS = -lm -fopenmp -lhwy
