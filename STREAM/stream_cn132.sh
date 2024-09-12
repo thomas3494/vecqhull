67daaf83dc3099434da861602bbd751b333a49c0
Thomas Koopman <tkoopman@slurm22.science.ru.nl>
2024-09-12 12:52:21 +0200

    Dynamic allocation, proper counting for BW

--- STREAM/stream_cn132.sh
+++ STREAM/stream_cn132.sh
@@ -5,8 +5,8 @@
 #SBATCH --mem=0
 #SBATCH --cpus-per-task=32
 #SBATCH --time=1:00:00
-#SBATCH --output=stream.out
+#SBATCH --output=stream_cn132.out
 
 make clean
-make
+make stream_c
 OMP_PLACES=cores numactl --interleave all ./stream_c
